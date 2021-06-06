98f3d066e52b12d5b6a19b58a8b382f154a6a29a
Simone <simone.denada@gmail.com>
2021-06-06 14:06:00 +0200

    made enhancements script interactive

--- enhancements.ps1
+++ enhancements.ps1
@@ -1,3 +1,6 @@
+#Requires -RunAsAdministrator
+
+
 # Ask for elevated permissions if required
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
@@ -5,43 +8,80 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 }
 
 
-########
-# Audio
-########
-
-# Set Communications Audio Ducking to "Do Nothing"
-Write-Host "Set Communications Audio Ducking to Do Nothing..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Multimedia\Audio" -Name "UserDuckingPreference" -Type DWord -Value 3
-
-
-########
-# Mouse
-########
-
-# Set Mouse to 1:1
-Write-Host "Set mouse speed to 1:1 value..."
-Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSensitivity" -Type String -Value "10"
-
-# Disable Mouse "Enhance pointer precision"
-Write-Host "Disabling Enhance pointer precision..."
-Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSpeed" -Type String -Value "0"
-
-
-##########################
-# General Personalization
-##########################
-
-# Hide "Recently added apps" from start menu
-Write-Host "Hide Recently added apps..."
-If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer")) {
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" | Out-Null
+# PSMenu is a required module to run this script
+if (!$(Get-Module -ListAvailable PSMenu)) {
+    Install-PackageProvider NuGet -Force;
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    Install-Module PSMenu -Repository PSGallery
 }
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "HideRecentlyAddedApps" -Type DWord -Value 1
 
-# Disable "Show suggestions occasionally in Start"
-Write-Host "Disabling Show suggestions occasionally in Start..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338388Enabled" -Type DWord -Value 0
 
-# Disable "Show recently opened files in Jump Lists"
-Write-Host "Disabling Show recently opened files in Jump Lists..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackDocs" -Type DWord -Value 0
+$enhancements_list = [Collections.Generic.List[Object]](@(
+    ########
+    # Audio
+    ########
+    [PSCustomObject]@{
+        name='Set Communications Audio Ducking to Do Nothing';
+        script='
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Multimedia\Audio" -Name "UserDuckingPreference" -Type DWord -Value 3;
+        '
+    }
+    ########
+    # Mouse
+    ########
+    [PSCustomObject]@{
+        name='Set mouse speed to 1:1 value';
+        script='
+            Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSensitivity" -Type String -Value "10"
+        '
+    }
+    [PSCustomObject]@{
+        name='Disabling Enhance pointer precision';
+        script='
+            Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSpeed" -Type String -Value "0"
+        '
+    }
+    ##########################
+    # General Personalization
+    ##########################
+    [PSCustomObject]@{
+        name='Hide Recently added apps';
+        script='
+            If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer")) {
+                New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" | Out-Null
+            }
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "HideRecentlyAddedApps" -Type DWord -Value 1
+        '
+    }
+    [PSCustomObject]@{
+        name='Disabling Show suggestions occasionally in Start';
+        script='
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338388Enabled" -Type DWord -Value 0
+        '
+    }
+    [PSCustomObject]@{
+        name='Disabling Show recently opened files in Jump Lists';
+        script='
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackDocs" -Type DWord -Value 0
+        '
+    }
+))
+
+
+function runEnhancementsSelection() {
+    echo ""
+    echo "Select the enhancements you want to apply, or press ESC to exit"
+    echo "(Move with ARROW KEYS, select with SPACEBAR, continue with ENTER)"
+    echo ""
+
+    $selected = menu $enhancements_list.name -Multiselect -ReturnIndex
+
+    foreach ($enhancement_index in $selected) {
+        $enhancement = $enhancements_list[$enhancement_index]
+        echo "Running: $($enhancement.name)"
+        iex $enhancement.script
+    }
+}
+
+
+runEnhancementsSelection
